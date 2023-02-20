51099e71e2bfc4f81b4186f9be4f9afec5f1b064
Liliia Paliichuk <113315152+lili2628@users.noreply.github.com>
2023-02-20 01:14:41 +0100

    added css changes

--- src/templates/flag-name-card.js
+++ src/templates/flag-name-card.js
@@ -3,7 +3,7 @@ export default function flagNameCardTpl (country){
     
     return `
         <li class=country-list-item>
-            <img class=flag-img src=${svg} alt="flag of ${official}" width=35>
+            <img class=flag-img src=${svg} alt="flag of ${official}" width=35 height=20 >
             <p class=card-name> ${official}
         </li>
     `;
